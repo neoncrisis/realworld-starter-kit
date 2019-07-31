27c7673d752db3a96a970e1aaa1a99a240905337
Oz Lopez <osvaldo.lopez@superluminal.me>
2019-07-30 21:38:57 -0600

    Add some test helpers

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 help: ## Print documentation
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
-ghcid: ## Run the server in development mode
+server: ## Run the server in development mode
 	@which ghcid > /dev/null || stack install ghcid
 	@ghcid \
 		--command "stack ghci haskell-servant-realworld" \
@@ -13,6 +13,12 @@ ghcid: ## Run the server in development mode
 		--warnings \
 		--test "DevelMain.update"
 
-# $(STACK_INSTALL_ROOT)/haskell-servant-realword: package.yaml stack.yaml
-# 	@echo "would compile"
-
+tests: ## Run the server in development mode
+	@which ghcid > /dev/null || stack install ghcid
+	@ghcid \
+		--command "stack ghci :spec" \
+		--restart "package.yaml" \
+		--restart "stack.yaml" \
+		--restart ".ghci" \
+		--warnings \
+		--test ":main"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,27 +1,78 @@
 {-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns             #-}
 {-# LANGUAGE OverloadedLabels           #-}
 
+import Control.Exception.Safe (MonadCatch, MonadThrow, bracket, try)
 import Control.Lens (use, (%=))
-import Control.Monad.State.Strict (MonadState, State)
+import Control.Monad.Except (ExceptT (..))
+import Control.Monad.State.Strict (MonadState, StateT, evalStateT)
 import Data.Generics.Labels ()
 import Data.Map (Map)
 import qualified Data.Map as Map
 import GHC.Generics (Generic)
+import Network.HTTP.Client (defaultManagerSettings, newManager)
+import Network.Wai.Handler.Warp (openFreePort, run)
+import RealWorld.Server (application)
 import RealWorld.Service.User (MonadUser (..))
 import RealWorld.Types.User (User)
+import Servant.Client (BaseUrl (..), ClientM, Scheme (Http), ServantError, mkClientEnv, runClientM)
+import Servant.Server (Handler (..))
+import Test.Tasty
+import Test.Tasty.HUnit
+import UnliftIO.Concurrent (ThreadId, forkIO, killThread)
+
+
+type Step =
+  String -> IO ()
+
+main :: IO ()
+main =
+  defaultMain tests
+
+tests :: TestTree
+tests =
+  testGroup "RealWorld"
+    [ testGroup "User"
+      [ testCaseSteps "login" testLoginUser
+      ]
+    ]
+
+testLoginUser :: Step -> Assertion
+testLoginUser step = withServer initialSt $ \srv -> do
+  step (show srv)
+  fail "not implemented"
+
+
+
+-- * State
 
 data St = St
   { users :: Map Int User
   } deriving (Show, Eq, Generic)
 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+initialSt :: St
+initialSt = St
+  { users = mempty
+  }
 
+
+-- * Test Monad
 
 newtype Test a = Test
-  { unTest :: State St a }
-  deriving (Applicative, Functor, Monad, MonadState St)
+  { unTest :: StateT St IO a }
+  deriving
+    ( Applicative
+    , Functor
+    , Monad
+    , MonadState St
+    , MonadThrow
+    , MonadCatch
+    )
+
+runTest :: St -> Test a -> Handler a
+runTest st a =
+  Handler $ ExceptT $ try $ evalStateT (unTest a) st
 
 
 instance MonadUser Test where
@@ -42,4 +93,33 @@ instance MonadUser Test where
 
   updateUser _u = do
     pure (error "Not implemented")
+
+
+-- * Server
+
+data Server = Server
+  { threadId :: ThreadId
+  , port     :: Int
+  } deriving (Eq, Show, Generic)
+
+withServer :: St -> (Server -> IO a) -> IO a
+withServer st =
+  bracket (runServer st) (killThread . threadId)
+
+runServer :: St -> IO Server
+runServer st = do
+  port <- fst <$> openFreePort
+  tid  <- forkIO
+    $ run port
+    $ application
+    $ runTest st
+  pure $ Server tid port
+
+runClient :: Server -> ClientM a -> IO (Either ServantError a)
+runClient Server{port} c = do
+  mgr <- newManager defaultManagerSettings
+  let
+    url = BaseUrl Http "localhost" port ""
+    env = mkClientEnv mgr url
+  runClientM c env
 
